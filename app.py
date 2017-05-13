9f4c7fc1e2eafff8542e28b176fdf266efebc8fc
ardlop <arieldl85@gmail.com>
2017-05-13 01:15:59 +0000

    updated scripts

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask
 from models import db, User
 import ConfigParser
 from flask_sqlalchemy import SQLAlchemy
+from socket import gethostname
 
 
 app = Flask(__name__)
-
+'''
 config = ConfigParser.ConfigParser()
 config.read('config.py')
 
@@ -16,24 +17,31 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostnam
         databasename =  config.get('DB', 'db'),)
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-
+'''
 '''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://' + config.get('DB', 'user') + \
 ':' + config.get('DB', 'password') + '@' + \
 config.get('DB', 'host') + '/' + config.get('DB', 'db')
 '''
+
+app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+        username="ardlop",
+        password="Elcoco.com",
+        hostname="ardlop.mysql.pythonanywhere-services.com",
+        databasename="ardlop$apiDB",)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-db = SQLAlchemy(app)
-
 
 @app.route('/')
 def hello_world():
-    return SQLALCHEMY_DATABASE_URI;
-    #return 'Hello from Flask! UPDATED!!'
+    #return SQLALCHEMY_DATABASE_URI;
+    return 'Hello from Flask! UPDATED!!'
 
-'''
+
 if __name__ == '__main__':
     db.create_all()
-    app.run()
-'''
+    if 'liveconsole' not in gethostname():
+        app.run()
+
