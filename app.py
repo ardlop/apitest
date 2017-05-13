9bc635c2ecda066daaf725cb80dc10d875f890da
Ariel Duarte López <arieldl85@gmail.com>
2017-05-13 02:05:21 +0200

    adding the config file and updating the app script

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask
+from models import db, User
+import ConfigParser
 
 app = Flask(__name__)
+
+onfig = ConfigParser.ConfigParser()
+config.read('config.py')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://' + config.get('DB', 'user') + \
+':' + config.get('DB', 'password') + '@' + \
+config.get('DB', 'host') + '/' + config.get('DB', 'db')
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello from Flask!'
 
+'''
+if __name__ == '__main__':
+    db.create_all()
+    app.run()
+'''
---
+++ config.py
@@ -0,0 +1,5 @@
+[DB]
+user: root
+password: root
+db: serverTest
+host: 127.0.0.1
